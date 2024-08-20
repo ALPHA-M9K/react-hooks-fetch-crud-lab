5cf40a554febba7be5ed3777a841bfb72bdf83a2
ALPHA ALVIN <alpha.muchemi@student.moringaschool.com>
2024-08-20 08:05:30 +0300

    crud

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,22 @@
+// import React, { useState } from "react";
+// import AdminNavBar from "./AdminNavBar";
+// import QuestionForm from "./QuestionForm";
+// import QuestionList from "./QuestionList";
+
+// function App() {
+//   const [page, setPage] = useState("List");
+
+//   return (
+//     <main>
+//       <AdminNavBar onChangePage={setPage} />
+//       {page === "Form" ? <QuestionForm /> : <QuestionList />}
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
